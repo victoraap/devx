df9173bd9ba7a5dafd7d3cf03e30e73675c367d7
VAlbites <42452634+victoraap@users.noreply.github.com>
2023-09-16 22:57:36 -0500

    fix fecha nac

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+const cheerio = require('cheerio'); // Añade esto para cargar y analizar el HTML
 
 const botToken = process.env.botToken;
 const apiToken = process.env.apiToken;
@@ -10,7 +11,41 @@ const apiUrl = process.env.apiUrl;
 
 const bot = new Telegraf(botToken);
 
-bot.hears(/\/dni|\.dni/, async (ctx)=> {
+// Función para extraer la fecha de nacimiento y la edad
+async function obtenerFechaNacimientoYEdad(dni, ctx) {
+  const url = "https://dni-peru.com/buscar-fecha-de-nacimiento-con-dni-peru/";
+  const data = `dni7=${dni}&fecha=Buscar`;
+
+  try {
+    const response = await axios.post(url, data);
+    const $ = cheerio.load(response.data);
+
+    const fechaNacimiento = $('.resultado p:contains("Fecha de Nacimiento:")').text();
+    const fechaNacimientoMatch = fechaNacimiento.match(/\d{2}\/\d{2}\/\d{4}/);
+
+    if (fechaNacimientoMatch) {
+      const fechaNacimientoStr = fechaNacimientoMatch[0];
+      const fechaNacimientoParts = fechaNacimientoStr.split('/');
+      const diaNacimiento = parseInt(fechaNacimientoParts[0]);
+      const mesNacimiento = parseInt(fechaNacimientoParts[1]) - 1;
+      const anoNacimiento = parseInt(fechaNacimientoParts[2]);
+
+      const fechaActual = new Date();
+      const edad = fechaActual.getFullYear() - anoNacimiento;
+
+      return { fechaNacimiento: fechaNacimientoStr, edad };
+    } else {
+      ctx.reply('No se encontró la fecha de nacimiento.');
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    ctx.reply('Ocurrió un error al obtener la fecha de nacimiento y la edad.');
+    return null;
+  }
+}
+
+bot.hears(/\/dni|\.dni/, async (ctx) => {
   const args = ctx.message.text.split(' ');
   if (args.length !== 2) {
     ctx.reply('El comando /dni debe tener un número de DNI después del espacio.');
@@ -36,42 +71,36 @@ bot.hears(/\/dni|\.dni/, async (ctx)=> {
     const data = response.data;
 
     if (data.success) {
-      const fechaNacimiento = new Date(data.data.fecha_nacimiento);
-      const fechaActual = new Date();
-      const diferenciaMilisegundos = fechaActual - fechaNacimiento;
-      const edadMilisegundos = new Date(diferenciaMilisegundos);
-      const edad = edadMilisegundos.getUTCFullYear() - 1970;
+      // Llama a la función para obtener la fecha de nacimiento y la edad
+      const fechaEdad = await obtenerFechaNacimientoYEdad(dni, ctx);
 
-      const messageText = `
-      \n\n⚜️ <b>Status</b>  -»  <code> Success ✅</code>
+      if (fechaEdad) {
+        const messageText = `
+        ⚜️ <b>Status</b>  -»  <code> Success ✅</code>
 ⚜️ <b>DNI</b>  -»  <code>${dni}</code>
-⚜️ <b>Nombres Completos</b> -»  <code>${data.data.nombre_completo}</code>
 ⚜️ <b>Nombres</b>  -» <code>${data.data.nombres}</code>
 ⚜️ <b>Apellido Paterno</b> -» <code>${data.data.apellido_paterno}</code>
 ⚜️ <b>Apellido Materno</b> -» <code>${data.data.apellido_materno}</code> 
-⚜️ <b>Edad</b> -» <code>${edad}</code> 
-⚜️ <b>Fecha Nac</b> -» <code>${data.data.fecha_nacimiento}</code>  
+⚜️ <b>Edad</b> -» <code>${fechaEdad.edad}</code> 
+⚜️ <b>Fecha Nac</b> -» <code>${fechaEdad.fechaNacimiento}</code>  
 ⚜️ <b>Codigo Verificación</b> -» <code>${data.data.codigo_verificacion}</code>  
-⚜️ <b>Estado Civil</b> -» <code>${data.data.estado_civil}</code> 
-⚜️ <b>Direccion</b> -» <code>${data.data.direccion_completa}</code>`;
+`;
+
+        ctx.replyWithHTML(messageText, {
+          reply_to_message_id: ctx.message.message_id,
+          allow_sending_without_reply: true
+        });
+      }
+    } else {
+      const messageText = `
+      ⚜️ <b>Status</b>  -»  <code> No Existe ❌</code>
+      ⚜️ <b>Response</b>  -»  <code>${data.message}</code>`;
 
       ctx.replyWithHTML(messageText, {
         reply_to_message_id: ctx.message.message_id,
         allow_sending_without_reply: true
       });
-    } else {
-      const messageText = `
-      \n\n⚜️ <b>Status</b>  -»  <code> No Existe ❌</code>
-⚜️ <b>Response</b>  -»  <code>${data.message}</code>`;
-
-
-          ctx.replyWithHTML(messageText, {
-            reply_to_message_id: ctx.message.message_id,
-            allow_sending_without_reply: true
-          });
     }
-
-    
   } catch (error) {
     console.error(error);
     ctx.reply('Ocurrió un error al consultar los datos del DNI.');
@@ -83,6 +112,7 @@ bot.hears(/\/dni|\.dni/, async (ctx)=> {
 bot.start((ctx) => {
   ctx.reply('¡Hola! Soy un bot de Telegram.');
 });
+
 
 bot.launch({
   webhook: {
